1a772990a27ca266777d322d88dd58581bf12f9a
RobertoDiazEc <robertodiazec@gmail.com>
2024-12-05 18:40:34 -0500

    cambio 1.1

--- rxconfig.py
+++ rxconfig.py
@@ -3,4 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="redx_web",
     api_url="https://api-redx-web.up.railway.app",
+    cors_allowed_origins=[
+        "http://localhost:3000"
+    ]
 )
