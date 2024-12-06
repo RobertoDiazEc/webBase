7675c29715f9d35caf251690d20c13fd9fb44bda
RobertoDiazEc <robertodiazec@gmail.com>
2024-12-06 14:13:23 -0500

    acceso al 3000 puerto

--- rxconfig.py
+++ rxconfig.py
@@ -5,6 +5,6 @@ config = rx.Config(
     #api_url="https://api-redx-web.up.railway.app",
     cors_allowed_origins=[
         "http://localhost:3000"
-        "https://redeveloped-web.vercel.app"
+        "https://redeveloped-web.vercel.app:3000"
     ],
 )
